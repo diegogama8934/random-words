2e612d868ad5d57dc781d5e7d82a316ccce44fb6
Diego Martinez <diegogama8934@gmail.com>
2024-08-21 09:21:25 -0600

    add: word context working well

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import { WordProvider } from "@/contexts/WordContext";
 import "./globals.css";
 
 const roboto = Roboto({ weight: ["100", "300", "400", "500", "700", "900"], subsets: ["latin"] });
@@ -19,7 +20,9 @@ export default function RootLayout({
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,1,0" />
       </head>
-      <body className={roboto.className}>{children}</body>
+      <WordProvider>
+        <body className={roboto.className}>{children}</body>
+      </WordProvider>
     </html>
   );
 }
