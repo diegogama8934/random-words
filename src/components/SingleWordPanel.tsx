fe4ea76e45d2bc8333ebf95bad0956079a96a540
Diego Martinez <diegogama8934@gmail.com>
2024-08-26 23:21:58 -0600

    add: ugly accordeon of word information

--- src/components/SingleWordPanel.tsx
+++ src/components/SingleWordPanel.tsx
@@ -7,7 +7,7 @@ export function SingleWordPanel({ wordInformation }: { wordInformation: WordResp
   return (
     <div className="p-16 flex flex-col gap-8 items-center">
       <h1 className="w-fit font-bold text-3xl bg-gradient-to-r from-violet-600 to-indigo-600 gradient-text capitalize">{wordInformation.word}</h1>
-      <main className="flex justify-center flex-wrap gap-8">
+      <main className="flex justify-center flex-col gap-8">
         {wordInformation.results.map((singleConcept, index) => <WordInformation key={index} {...singleConcept} />)}
       </main>
     </div>
